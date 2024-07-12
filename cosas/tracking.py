fe8302d7a08f15eea1d06b1afe2ce3cd0e1d83ec
4pygmalion <4pygmalion@naver.com>
2024-07-12 10:38:53 +0900

    input, gt, prediction plotting 함수 추가 9Issue #11)

--- cosas/tracking.py
+++ cosas/tracking.py
@@ -1,4 +1,10 @@
+import os
 import mlflow
+import numpy as np
+
+import torch
+from .misc import plot_xypred
+import matplotlib.pyplot as plt
 
 TRACKING_URI = "http://219.252.39.224:5000/"
 EXP_NAME = "cosas"
@@ -15,3 +21,22 @@ def get_experiment(experiment_name=EXP_NAME):
         return client.get_experiment_by_name(experiment_name)
 
     return experiment
+
+
+def plot_and_save(
+    image_name: str,
+    original_x: np.ndarray,
+    original_y: tuple,
+    pred_y: torch.Tensor,
+    artifact_dir: str,
+):
+    temp_save_path = f"{image_name}.png"
+    fig, axes = plot_xypred(original_x, original_y, pred_y)
+    fig.savefig(temp_save_path)
+
+    mlflow.log_artifact(temp_save_path, artifact_dir)
+
+    os.remove(temp_save_path)
+    plt.close(fig)
+    plt.clf()
+    plt.cla()
