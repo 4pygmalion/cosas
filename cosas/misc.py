2ada0f1e17c2135d37829560b62efa4292018b82
Ho Heon Kim <45510932+4pygmalion@users.noreply.github.com>
2024-07-17 15:27:53 +0900

    n_patience default = 10

--- cosas/misc.py
+++ cosas/misc.py
@@ -27,9 +27,8 @@ def get_config() -> argparse.ArgumentParser:
     parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
     parser.add_argument("--device", type=str, default="cuda", help="Device to use")
     parser.add_argument("--lr", type=float, default=0.001, help="Learning rate")
-
     parser.add_argument(
-        "--n_patience", type=int, default=7, help="Number of patience epochs"
+        "--n_patience", type=int, default=10, help="Number of patience epochs"
     )
 
     return parser.parse_args()
