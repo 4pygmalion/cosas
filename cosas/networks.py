b6d0df1a5c39bed69230f7148909c143f626226e
4pygmalion <4pygmalion@naver.com>
2024-07-23 11:11:32 +0900

    efficient net b7 통일 (issue #24)

--- cosas/networks.py
+++ cosas/networks.py
@@ -11,12 +11,12 @@ class PyramidSeg(torch.nn.Module):
         self.level1_size = (224 * 3, 224 * 3)
         self.level2_size = (224, 224)
         self.level0 = smp.FPN(
-            encoder_name="efficientnet-b4",
+            encoder_name="efficientnet-b7",
             encoder_weights="imagenet",
             classes=2,
         )
         self.level0.encoder._conv_stem = Conv2dStaticSamePadding(
-            3, 48, kernel_size=(3, 3), stride=(2, 2), bias=False, image_size=224
+            3, 64, kernel_size=(3, 3), stride=(2, 2), bias=False, image_size=224
         )
         self.level1 = smp.FPN(
             encoder_name="efficientnet-b7",
@@ -24,7 +24,7 @@ class PyramidSeg(torch.nn.Module):
             classes=2,
         )
         self.level1.encoder._conv_stem = Conv2dStaticSamePadding(
-            5, 48, kernel_size=(3, 3), stride=(2, 2), bias=False, image_size=224
+            5, 64, kernel_size=(3, 3), stride=(2, 2), bias=False, image_size=224
         )
         self.level2 = smp.FPN(
             encoder_name="efficientnet-b7",
@@ -53,7 +53,7 @@ class PyramidSeg(torch.nn.Module):
                 [x.unsqueeze(0), level0_output.unsqueeze(0)], axis=1
             )
 
-            downsampled_o = Resize(self.level1_size)(level0_fusion).unsqueeze(0)
+            downsampled_o = Resize(self.level1_size)(level0_fusion)
             level1_inputs = tesellation(downsampled_o)  # (1, N, C, 224*3, 224*3)
             level1_output = self.level1(level1_inputs.squeeze(0))
             level1_output = reverse_tesellation(
