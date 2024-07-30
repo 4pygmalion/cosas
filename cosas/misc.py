cee2a390e16520ff4a5aa77fedbc58fe70eb3c67
4pygmalion <4pygmalion@naver.com>
2024-07-30 14:58:06 +0900

    최대 epochs 200으로 증가

--- cosas/misc.py
+++ cosas/misc.py
@@ -32,7 +32,7 @@ def get_config() -> argparse.ArgumentParser:
     )
     parser.add_argument("--run_name", type=str, default="baseline", help="Run name")
     parser.add_argument("--seed", type=int, default=42, help="Random seed")
-    parser.add_argument("--epochs", type=int, default=50, help="Number of epochs")
+    parser.add_argument("--epochs", type=int, default=200, help="Number of epochs")
     parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
     parser.add_argument("--device", type=str, default="cuda", help="Device to use")
     parser.add_argument(
