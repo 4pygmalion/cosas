aa47c2cb4d304e11962344a782c1cf232673b117
4pygmalion <4pygmalion@naver.com>
2024-08-09 16:01:13 +0900

    테스트용 AutoEncoder + EfficientNet 모듈로 작성 (Issue #48)

--- inference.py
+++ inference.py
@@ -1,9 +1,11 @@
 import os
-import np
-import torch
-import mlflow
-import SimpleITK #대회 주최 측에서 .mha 파일 쓰는데 문제 없다고 함
 from typing import List
+
+import numpy as np
+import torch
+import SimpleITK 
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -11,35 +13,46 @@ def read_image(path) -> List[np.ndarray]:
     image = SimpleITK.ReadImage(path)
     return SimpleITK.GetArrayFromImage(image)
 
-# TODO
-def preprocess_image(image_array):
+def preprocess_image(image_array:np.ndarray):
     """[summary] preprocessing input image into model format"""
-    return
+    
+    # TODO: 하드코딩이 아니라, A.Compose을 직렬화해서 불러올수 있도록
+    transform = A.Compose(
+        [
+            A.Resize(384, 384),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(),
+        ]
+    )
+    
+    return transform(image=image_array)['image'].to("cuda").unsqueeze(0)
 
-# TODO
-def postprocess_image(image_array):
+def postprocess_image(image_array:np.ndarray, original_size):
     """[summary] postprocessing model result to original image format"""
-    return
+    
+    transform = A.Compose(
+        [
+            A.Resize(*original_size),
+        ]
+    )
+    return transform(image=image_array)['image']
 
-# TODO
 def write_image(path, result):
-    # 주최 제공 템플릿
     image = SimpleITK.GetImageFromArray(result)
     SimpleITK.WriteImage(image, path, useCompression=False)
     return 
 
 
 def main():
-    # 대회 주최측에서 제공한 input/output 경로 (output 있는지 확인해달라고 함)
+    
     input_dir = '/input/images/adenocarcinoma-image'
     output_dir = '/output/images/adenocarcinoma-mask'
-
     os.makedirs(output_dir, exist_ok=True)
 
     # 모델 load 
-    model_path = os.path.join(CURRENT_DIR, "checkpoint.pth")
-    model = torch.load(model_path)
-
+    model_path = os.path.join(CURRENT_DIR, "model.pth")
+    model = torch.load(model_path).eval()
+        
     with torch.no_grad():
         for filename in os.listdir(input_dir):
             if filename.endswith('.mha'):
@@ -47,12 +60,15 @@ def main():
                 try:
                     input_path = os.path.join(input_dir, filename)
                     raw_image = read_image(input_path)
-                    image = preprocess_image(raw_image)
-
-                    raw_result = model.eval(image)
-                    #TODO
-                    result = postprocess_image(raw_result)
+                    original_size = raw_image.shape[-2:]
+                    
+                    x:torch.Tensor = preprocess_image(raw_image)
+                    pred = model(x)
+                    pred_mask = pred["mask"].cpu().numpy()[0]
+                    
+                    result = postprocess_image(pred_mask, original_size=original_size)
                     write_image(output_path, result)
+                    
                 except Exception as error:
                     print(error)
 
