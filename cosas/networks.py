869742728665f41069ce768c3a84e55e7cc39250
4pygmalion <4pygmalion@naver.com>
2024-09-03 09:26:45 +0900

    funsion with linear operation

--- cosas/networks.py
+++ cosas/networks.py
@@ -1069,9 +1069,7 @@ class EnsembleModel_Segform_MTaskAE(torch.nn.Module):
             def __init__(self):
                 super(FusionModule, self).__init__()
                 self.layer = torch.nn.Sequential(
-                    torch.nn.Conv2d(2, 8, kernel_size=(1, 1), stride=(1, 1)),
-                    torch.nn.ReLU(),
-                    torch.nn.Conv2d(8, 1, kernel_size=(1, 1), stride=(1, 1)),
+                    torch.nn.Conv2d(2, 1, kernel_size=(1, 1), stride=(1, 1)),
                 )
 
             def forward(self, x):
